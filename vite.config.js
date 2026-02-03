dabf0792c663491c3f48b6598e04319ffc24e116
Yixue8924 <yixue8924@gmail.com>
2026-02-03 14:23:11 +0800

    Revise package.json for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     build: {
-        minify: 'terser',
         rollupOptions: {
             output: {
                 manualChunks: {
